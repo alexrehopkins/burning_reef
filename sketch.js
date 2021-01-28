46fa639622f53cf96940c24a294990af9d46293c
alexrehopkins <lxyoshi@gmail.com>
2021-01-28 00:48:43 +0000

    skybox ye

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { FirstPersonControls } from './node_modules/three/examples/jsm/controls/FirstPersonControls.js';
 import { ImprovedNoise } from './node_modules/three/examples/jsm/math/ImprovedNoise.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 let container;
-let camera, controls, scene, renderer;
+let camera, controls, scene, renderer, coral, hlight, directionalLight;
 let mesh, texture;
+let timeLeft = 100; //percentage, 100 just started, 0 is entirely degraded and ending
 
 const worldWidth = 56, worldDepth = 56;
 const clock = new THREE.Clock();
@@ -16,18 +18,18 @@ function init() {
 
     container = document.getElementById( 'container' );
 
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 ); //last value is render distance
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x13aae8 );
-    scene.fog = new THREE.FogExp2( 0x13aae8, 0.001 );
+    scene.background = new THREE.Color( 0x29a4d2 );
+    scene.fog = new THREE.FogExp2( 0x29a4d2, 0.001 ); //0.001 for fog
 
     const data = generateHeight( worldWidth, worldDepth );
 
     camera.position.set( 100, 800, - 800 );
     camera.lookAt( - 100, 810, - 800 );
 
-    const geometry = new THREE.PlaneBufferGeometry( 7500, 7500, worldWidth - 1, worldDepth - 1 );
+    const geometry = new THREE.PlaneBufferGeometry( 10000, 10000, worldWidth - 1, worldDepth - 1 );
     geometry.rotateX( - Math.PI / 2 );
 
     const vertices = geometry.attributes.position.array;
@@ -37,6 +39,50 @@ function init() {
         vertices[ j + 1 ] = data[ i ] * 10;
 
     }
+
+    hlight = new THREE.AmbientLight (0x404040,2);
+    scene.add(hlight);
+    directionalLight = new THREE.DirectionalLight(0xffffff,1);
+    directionalLight.castShadow = true;
+    directionalLight.position.set(0,1000,0);
+    scene.add(directionalLight);
+
+
+    //coral import
+    loadCoral('assets/coral1Coloured.gltf',2);
+    loadCoral('assets/coral2Coloured.gltf',1);
+    loadCoral('assets/kelp1Coloured.gltf',4);
+    loadCoral('assets/seaweed1Coloured.gltf',3);
+
+
+    //skybox
+    let materialArray = [];
+    let texture_nz = new THREE.TextureLoader().load('assets/nz.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_nz}));
+    let texture_nx = new THREE.TextureLoader().load('assets/nx.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_nx}));
+    let texture_py = new THREE.TextureLoader().load('assets/py.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_py}));
+    let texture_ny = new THREE.TextureLoader().load('assets/ny.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ny}));
+    let texture_px = new THREE.TextureLoader().load('assets/px.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_px}));
+    let texture_pz = new THREE.TextureLoader().load('assets/pz.png');
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_pz}));
+
+    for(let i=0;i<6;i++){
+        materialArray[i].side = THREE.BackSide;
+        materialArray[i].fog = false;
+    }
+
+    let skyboxGeo = new THREE.BoxGeometry(7500,7500,7500);
+    let skyboxV = new THREE.Mesh(skyboxGeo, materialArray);
+    scene.add(skyboxV)
+
+
+    //end of skybox
+
+
 
     texture = new THREE.CanvasTexture( generateTexture( data, worldWidth, worldDepth ) );
     texture.wrapS = THREE.ClampToEdgeWrapping;
@@ -51,7 +97,7 @@ function init() {
     container.appendChild( renderer.domElement );
 
     controls = new FirstPersonControls( camera, renderer.domElement );
-    controls.movementSpeed = 500;
+    controls.movementSpeed = 100;
     controls.lookSpeed = 0.05;
     controls.verticalMax = 3*Math.PI/4;
     controls.verticalMin = Math.PI/4;
@@ -133,9 +179,9 @@ function generateTexture( data, width, height ) {
 
         shade = vector3.dot( sun );
 
-        imageData[ i ] = ( shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
-        imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-        imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i ] = ( 32 + shade * 32 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 1 ] = ( 96 + shade * 24 ) * ( 0.5 + data[ j ] * 0.007 );
+        imageData[ i + 2 ] = ( 64 +  shade * 24 ) * ( 0.5 + data[ j ] * 0.007 );
 
     }
 
@@ -181,10 +227,43 @@ function animate() {
 
 }
 
-
 function render() {
 
     controls.update( clock.getDelta() );
+    
     renderer.render( scene, camera );
+    
+    //timeline
+    if (timeLeft > 0) {
+        timeLeft = timeLeft - 0.01;
+        var elem = document.getElementById("myBar");
+        elem.style.width = (100-timeLeft) + "%";
+    }
+    
+}
+
+function loadCoral(assetLocation,scaler) {
+    const loader = new GLTFLoader();
+    loader.load(assetLocation,function ( gltf ) {
+        coral = gltf.scene;    
+        coral.scale.set(scaler,scaler,scaler);
+        groundDetect();
+        coral.position.set(80,800,-800);
+        scene.add( gltf.scene );
+               
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    
+        },
+        // called when loading has errors
+        function ( error ) {
+            console.log( 'An error happened ' + error );
+        }
+    );
+}
+
+function groundDetect() {
 
 }
