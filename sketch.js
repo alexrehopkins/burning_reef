900ca31f2295081d473997c2b294b12b937b04f9
alexrehopkins <51701886+alexrehopkins@users.noreply.github.com>
2021-04-08 23:12:51 +0100

    fixed mob lag

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let artefactX = 0;
 let artefactZ = 300;
 let artefact = [];
 let currentArtefact = 0; //none currently in environment, 1 = artefact1, -99 means none left to find
-let artefactFound = [0,0,0,0,0,0,0];
+let artefactFound = [1,1,1,1,1,1,0]//[0,0,0,0,0,0,0];
 let newcoral = 0;
 let data;
 let sky;
@@ -21,10 +21,11 @@ let resetCounter = 0;
 let spaceBetweenPoints;
 let meshes = [];
 let matrix = new THREE.Matrix4();
-let gameState = -2; //-2, loading assets, -1 title screen, 0 playing, 1 ending screen
+let gameState = -2; //-2, loading assets, -1 title screen, 0 playing, 1 ending screen opening, 2 congratulations, 3 opened, 4 return to main menu
 let music = new Audio('assets/underwatermusic.mp3');
 let tune = new Audio('assets/artefact.mp3');
 let artefactGlow = 0;
+let bobMultiplier = 1; // artefact bobbing tracker 1 means bobbing up, -1 bobbing down
 
 //also best left the same
 const worldDirectWidth = 56000, worldDirectDepth = 56000;
@@ -34,6 +35,26 @@ const clock = new THREE.Clock();
 //these two values are best kept the same, and the lower the less intensive on the gpu
 const respawnDistance = 200;
 const numIndividualAssets = 200;
+
+
+//ui
+let shown = -1; //-9: negative values mean it is disabled, 1-9 are values in the array of artefacts corresponding to descriptions etc
+let titles = ["Not yet Found","Plastic Usage","Sustainable Fishing","Ocean Products","Reduce Waste","Pollutants","Acidification","Respect Habitats"];
+let descriptions = [
+"Discover this artefact first to learn more about its impacts and how we can help our oceans!",
+"The oceans face a huge threat from plastics. It's estimated over 17 billion pounds of plastic leaks into the ocean environment from the land every year, only by reducing our use of plastic based products will companies stop producing the waste. Save oceans and demand plastic-free alternatives, such as paper straws and bags instead of plastic, or even better bring your own reusable items along with you so you don’t have to buy disposable items that you will only get rid of later!",
+"With global food consumption only growing, some fisheries resort to overfishing and tipping the ecosystem balance. Try to buy organic seafood and consider adding small, oily fish such as anchovies and sardines to your diet that are not only packed with protein but are more sustainable and in abundance in the wild, unlike the heavily in demand salmon which must now be farmed. If fishing yourself, consider the ‘catch and release’ approach to keep the fish population alive and bustling!",
+"Many products are directly linked to unsustainable fishing methods and pollution that all harm endangered species and the ocean as a whole. A few examples of contributors are cosmetic manufacturers that use shark squalene in products, or souvenir manufacturers that use conch, turtle shells and especially coral for jewellery and decoration. Try to be vocal in spreading awareness to your community and encourage friends and family to adopt similar changes lifestyle and purchasing habits!",
+"Coastlines are becoming an increasingly prime location for tourists to gather and be merry, but as the crowds grow, so does their waste. When out at the beach and just in general, pay careful attention to leave no waste behind that might blow into the oceans or be taken away by the tide, as it will end up directly harming the oceans and poisoning fish, the same fish that we may end up eating. It’s important to keep in mind that what effects the ocean will come back around and affect our lifestyle!",
+"Pollution already has a huge effect on the environment as a whole, having both direct and indirect effects on Ocean life. Try to use non-toxic detergents around the house or dispose of toxic cleaning products properly and safely so they don’t end up polluting the waters. What might seem unsuspecting can be deadly to wildlife, such as plastic microbeads found in toothpaste and facial scrubs which are designed to wash down the drain but have been found ending up inside fish from the Arctic!",
+"The oceans acid levels rise with the amount of carbon dioxide in the atmosphere due to a chemical reaction from the air with the seawater. Much of the ocean wildlife is unable to survive in conditions too acidic, including making plankton and coral unable grow and even can dissolve shells of existing creatures. Try to reduce your carbon footprint by supporting cleaner energy sources and using less electricity and fuel. A few easy tips are to use public transport and check your home is well insulated!",
+"While the oceans may seem infinitely huge and resilient, many areas and habitats are formed around a fine balance where the wildlife can only survive in specific conditions and temperatures. If exploring around wildlife be careful not to disturb the area, and if boating make sure to anchor in sandy areas far from coral and sea grasses, by adhering to ‘no wake’ zones. Consider volunteering in your local area for beach clean-ups if you want to get involved in making a positive impact!"
+];
+
+let imgurls = ["assets/iconNotFound.jpg","assets/artefacts/a1.png","assets/artefacts/a2.png","assets/artefacts/a3.png","assets/artefacts/a4.png","assets/artefacts/a5.png","assets/artefacts/a6.png","assets/artefacts/a7.png"];
+
+
+
 
 
 
@@ -274,7 +295,7 @@ function animate() {
         }   
     }
     resetCounter++;
-    if (resetCounter >= numIndividualAssets) { //run every 50 frames
+    if (resetCounter >= numIndividualAssets) {
         resetCounter = 0;    
     }   
     
@@ -289,12 +310,20 @@ function animate() {
 }
 
 function render() {
-    if (controls.movementSpeed > respawnDistance/2) {
+    if (shown > 0){
+        controls.movementSpeed = 0;
+        controls.lookSpeed = 0;
+    }
+    else if (controls.movementSpeed > respawnDistance/2) {
         camera.fov = ((controls.movementSpeed-(respawnDistance/2))/16)+(respawnDistance/4);
         camera.updateProjectionMatrix();
         controls.movementSpeed--;
-    } else {
+        controls.lookSpeed = 0.07;
+    }
+    else {
         controls.autoForward = false;
+        controls.movementSpeed = respawnDistance/2;
+        controls.lookSpeed = 0.07;
     }//boost mechanic
 
     for (let i = 0; i<newcoral;i++){
@@ -306,19 +335,24 @@ function render() {
     music.playbackRate = 1+(0.4-(timeLeft/100));
     renderer.render( scene, camera );
     
-    if (currentArtefact == -99) {
-        gameState = 1;
-        ending();
+    if (currentArtefact == -99 && gameState < 2) {
+        ending(1);
     }
-
     //timeline
-    if (timeLeft < 100 && gameState == 0) {
+    if (timeLeft < 100 && gameState == 0 && shown < 0) {
         compassPointer();
-        timeLeft = timeLeft + 0.01; //every frame degrades
+        for (let i = 0; i < artefactFound.length; i++) {
+            if (artefactFound[i] == 1) {
+                timeLeft = timeLeft + 0.002; //every frame degrades
+            }
+        }
+        timeLeft = timeLeft + 0.001; //every frame degrades
         var elem = document.getElementById("myBar");
         handleArtefact();
         elem.style.width = (timeLeft) + "%";
         //document.getElementById("container").style.filter = "grayscale("+timeLeft/100+")";
+    } else if (timeLeft >= 100 && gameState < 2) {
+        ending(0);
     }
 }
 
@@ -379,32 +413,45 @@ function handleArtefact() {
             }
         }
     }
-
-    artefactGlow++;
-    if (artefactGlow > 15) {
-        artefactGlow=-15;
-    }
-    artefact[currentArtefact-1].traverse((o) => {
-        if (o.isMesh) {
-            Math.abs(artefactGlow);
-            o.material.emissive = new THREE.Color("hsl(0, 0%, "+Math.abs(artefactGlow)+"%)");
+    if (currentArtefact > -50) {
+    //bobbing
+        if (currentArtefact == 1 || currentArtefact == 2) {
+            if (resetCounter == 0) {
+                bobMultiplier = bobMultiplier * -1;
+            }
+            artefact[currentArtefact-1].position.y = artefact[currentArtefact-1].position.y + bobMultiplier/10;
         }
-      });
-    //collision and placement
-    let multiplier = 1;
-    if (currentArtefact == 4) {
-        multiplier = 3;
-    }
-    if ( Math.hypot(artefactX-camera.position.x,artefactZ-camera.position.z) < 70*multiplier ){
-        artefact[currentArtefact-1].position.y = -1000;
-        artefactFound[currentArtefact-1] = 1;
-        currentArtefact = 0;
-        artefactNotification();
-        music.pause;
 
-        //boost, remove at end?
-        //controls.autoForward = true;
-        //controls.movementSpeed = respawnDistance*2;
+
+        artefactGlow++;
+        if (artefactGlow > 15) {
+            artefactGlow=-15;
+        }
+        
+        
+        //collision and placement
+        let multiplier = 1;
+        if (currentArtefact == 4) {
+            multiplier = 3;
+        }
+        if ( Math.hypot(artefactX-camera.position.x,artefactZ-camera.position.z) < 70*multiplier ){
+            artefact[currentArtefact-1].position.y = -1000;
+            artefactFound[currentArtefact-1] = 1;
+            artefactNotification(currentArtefact);
+            currentArtefact = 0;
+            music.pause;
+
+            //boost, remove at end?
+            //controls.autoForward = true;
+            //controls.movementSpeed = respawnDistance*2;
+        } else {
+            artefact[currentArtefact-1].traverse((o) => {
+                if (o.isMesh) {
+                    Math.abs(artefactGlow);
+                    o.material.emissive = new THREE.Color("hsl(0, 0%, "+Math.abs(artefactGlow)+"%)");
+                }
+            });
+        }
     }
 }
 
@@ -497,10 +544,10 @@ function floorCollision() {
     let cameraZ = Math.round((halfOfDistanceDepth+camera.position.z)/spaceBetweenPoints)*spaceBetweenPoints;
     let floorY = 10*data[(cameraZ)/(spaceBetweenPoints)*worldWidth+cameraX/(spaceBetweenPoints)];
     floorY = camera.position.y-(floorY+70);
-    if (floorY < 0) {
+    if (floorY < 0 && gameState == 0) {
         camera.position.y += - floorY*0.04;   
     }
-    if (floorY > 500) { //if difference between floor and cam is greater than 500 then alter by a portion of the difference
+    if (floorY > 500 && gameState == 0) { //if difference between floor and cam is greater than 500 then alter by a portion of the difference
         camera.position.y += - (floorY-500)*0.04;   
     }
 }
@@ -519,17 +566,27 @@ function beginAV() {
     document.getElementById("info").style = "display: none;";
     document.getElementById("myBar").style = "display: block;";
     document.getElementById("menuOpener").style = "display: block;";
+    document.getElementById("container").style = "opacity: 1";
+    document.getElementById('compass').style = "display: block";
 }
 
 function startAnim() {
     if (gameState == -2 && (artefact.length+meshes.length)*4 == loadingArray.length){
         gameState = -1;
+        document.getElementById("enterButton").classList.remove("introd");
+        void document.getElementById("enterButton").offsetWidth; 
+        document.getElementById("enterButton").classList.add("introd");
+        document.getElementById("container").classList.remove("introd");
+        void document.getElementById("container").offsetWidth; 
+        document.getElementById("container").classList.add("introd");
+        
     }
     if (gameState == -1 && timeLeft < 1) {
         timeLeft += 0.01;
-        document.getElementById("enterButton").style = "opacity: "+timeLeft;
-        document.getElementById("container").style = "opacity: "+timeLeft;
-        document.getElementById("enterButton").style = "top: "+((timeLeft*80)-40)+"%";
+        //document.getElementById("enterButton").style = "opacity: "+timeLeft;
+        //document.getElementById("container").style = "opacity: "+timeLeft;
+        //document.getElementById("enterButton").style = "top: "+((timeLeft*80)-40)+"%";
+        
     }
 }
 
@@ -537,19 +594,42 @@ function compassPointer() {
     let vector = camera.getWorldDirection(new THREE.Vector3());
     let theta = THREE.Math.radToDeg(Math.atan2(vector.x,vector.z));
     let theta2 = THREE.Math.radToDeg(Math.atan2(camera.position.x-artefactX,camera.position.z-artefactZ));
-    document.getElementById('compass').style = "transform: rotateX("+(45+THREE.Math.radToDeg(vector.y))+"deg); display: block;";
+    //document.getElementById('compass').style = "transform: rotateX("+(45+THREE.Math.radToDeg(vector.y))+"deg);";
     document.getElementById('pointer').style = "transform: rotate("+(-theta2+theta)+"deg)";
 }
 
-function ending() {
-    console.log('END');
+function ending(win) {
+    gameState = 2;
+    controls.movementSpeed = 0;
+    controls.lookSpeed = 0;
+    updatePage(1);
+    //get total
+    let total = 0;
+    for (let i = 0; i < artefactFound.length; i++) {
+        if (artefactFound[i] == 1) {
+            total++
+        }
+    }
     document.getElementById("menu").style = "display: block;";
+    shown = 1;
+    if (win == 1) {
+        document.getElementById("info").innerHTML = "Well done! You managed to collect all the artefacts before the ocean degraded!";
+    }
+    else if (win == 0) {
+        document.getElementById("info").innerHTML = "You ran out of time. The ocean is beyond repair, but you did managed to collect "+total+" out of "+artefactFound.length+" of the artefacts!";
+    }
+    document.getElementById("info").style = "display: block;";
+    document.getElementById("myBar").style = "display: none;";
+    document.getElementById("compass").style = "display: none;";
     document.getElementById("menuOpener").innerHTML = "RETURN";
     document.getElementById("container").style = "opacity: 0";
 }
 
-function artefactNotification() {
-    document.getElementById("collection").style.animation = "notification 5s linear";
+function artefactNotification(source) {
+    document.getElementById("colImg").src = "assets/artefacts/a"+source+".png";
+    document.getElementById("collection").classList.remove("notif");
+    void document.getElementById("collection").offsetWidth; 
+    document.getElementById("collection").classList.add("notif");
 }
 
 function bleaching(coralN) {
@@ -561,6 +641,53 @@ function bleaching(coralN) {
     });
 }
 
-document.getElementById('enterButton').addEventListener("click", function() {beginAV()}, false);
 
-export default {artefactFound};
+function openMenu() {
+    if (shown < 0) {
+        document.getElementById("menu").style = "display: block;";
+        shown = shown*-1;
+        updatePage(1);
+        
+    }
+    else if (shown >= 0) {
+        document.getElementById("menu").style = "display: none;";
+        shown = shown*-1;
+    }
+
+    if (gameState > 1) {
+        location.reload();
+    }
+}
+
+function updatePage(pageIncrement) {
+    
+    for (let i = 1; i <= artefactFound.length; i++) {
+        //document.getElementById("art"+i).style.width = "";
+        //document.getElementById("art"+i).style.height = "";
+        document.getElementById("art"+i).style.filter = "sepia(100%)";
+        if (artefactFound[i-1] == 1) {
+            document.getElementById("art"+i).src = imgurls[i];
+            document.getElementById("art"+i).style.filter = "sepia(0%)";
+        }
+    }
+
+    //document.getElementById("art"+pageIncrement).style.width = "15vw";
+    //document.getElementById("art"+pageIncrement).style.height = "15vw";
+    if (artefactFound[pageIncrement-1] == 0) {
+        document.getElementById("title").innerHTML = titles[0];
+        document.getElementById("description").innerHTML = descriptions[0];
+    } else {
+        document.getElementById("title").innerHTML = titles[pageIncrement];
+        document.getElementById("description").innerHTML = descriptions[pageIncrement];
+    }
+}
+
+document.getElementById('menuOpener').addEventListener("click", function() {openMenu()}, false);
+document.getElementById('art1').addEventListener("click", function() {updatePage(1)}, false);
+document.getElementById('art2').addEventListener("click", function() {updatePage(2)}, false);
+document.getElementById('art3').addEventListener("click", function() {updatePage(3)}, false);
+document.getElementById('art4').addEventListener("click", function() {updatePage(4)}, false);
+document.getElementById('art5').addEventListener("click", function() {updatePage(5)}, false);
+document.getElementById('art6').addEventListener("click", function() {updatePage(6)}, false);
+document.getElementById('art7').addEventListener("click", function() {updatePage(7)}, false);
+document.getElementById('enterButton').addEventListener("click", function() {beginAV()}, false);
